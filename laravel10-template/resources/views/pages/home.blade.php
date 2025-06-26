d0b0c87bdc756b7c2b84435a48202a1a9d5ae370
SalvatoreGiacomazzo16 <salvatore161giacomazzo@gmail.com>
2025-06-26 18:09:22 +0200

    created table

--- laravel10-template/resources/views/pages/home.blade.php
+++ laravel10-template/resources/views/pages/home.blade.php
@@ -1,9 +1,32 @@
 @extends('layouts.app')
-@section("title", "homepage")
+@section("title", "Homepage")
 @section("main-content")
 
-@foreach($products as $product)
-{{ $product->id }}
-{{$product->name}}
-@endforeach
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Barcode</th>
+            <th scope="col">Brand</th>
+            <th scope="col">Category</th>
+            <th scope="col">Weight</th>
+            <th scope="col">Price</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($products as $product)
+        <tr>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->barcode }}</td>
+            <td>{{ $product->brand }}</td>
+            <td>{{ $product->category }}</td>
+            <td>{{ $product->weight }}</td>
+            <td>{{ $product->price }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+{{ $products->links() }}
+
 @endsection
